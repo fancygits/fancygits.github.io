a5c01c80fffdef87f211575537dff941ba1fdbd6
James Luke Johnson <jjohn144@my.westga.edu>
2017-12-01 22:13:46 -0600

    portfolio image gallery finished!

---
+++ scripts/portfolio.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	updateHeight();
+	$('#portfolio a').click(function() {
+		var portfolioHref = $(this).attr('href');
+		var portfolioAlt = $(this).attr('title');
+		//$('figure').css('display', 'none');
+		$('figure img').attr({ src: portfolioHref, alt: portfolioAlt }); 
+		$('figcaption').html(portfolioAlt);
+		//$('figure').fadeToggle(1000);
+		return false;
+	});
+});
+
+$(window).ready(updateHeight);
+$(window).resize(updateHeight);
+
+function updateHeight() {
+	var div = $('.img-hover');
+	var width = div.width();
+	div.css('height', width * 0.69);
+}
